8f03cd4309b88782f19f3c61e2b69f77cc921bd5
111111111111111111111111111WIFI <152071727+111111111111111111111111111WIFI@users.noreply.github.com>
2023-11-27 18:50:00 +1000

    1.4
    # Создание экземпляра приложения
    # Определение структуры дашборда

--- 111111111111111111.py
+++ 111111111111111111.py
@@ -13,3 +13,27 @@ df = pd.read_csv(file_path)
 df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])
 df['Delivery Date'] = pd.to_datetime(df['Delivery Date'], errors='coerce')
 df['Order Date'] = pd.to_datetime(df['Order Date'], errors='coerce')
+
+# Создание экземпляра приложения
+app = dash.Dash(__name__)
+
+# Определение структуры дашборда
+app.layout = html.Div([
+    html.H1('Данные транзакции', style={'textAlign': 'center'}),
+    dcc.Dropdown(
+        id='time-period-dropdown',
+        options=[
+            {'label': 'Месяц', 'value': 'M'},
+            {'label': 'Квартал', 'value': 'Q'},
+            {'label': 'Год', 'value': 'Y'}
+        ],
+        value='M',
+        clearable=False,
+        style={'width': '50%', 'margin': '0 auto'}
+    ),
+    dcc.Graph(id='time-series-chart'),
+    dcc.Graph(id='pie-chart'),
+    dcc.Graph(id='histogram'),
+    dcc.Graph(id='scatter-plot'),
+    html.Div(id='data-table')
+], style={'padding': '20px'})
