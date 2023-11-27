ea39c3a9eaf9be01e06cade5aa1dbd4e75a6717b
111111111111111111111111111WIFI <152071727+111111111111111111111111111WIFI@users.noreply.github.com>
2023-11-27 18:46:59 +1000

    1.3
    Добавлено преобразование столбцов с датами

--- 111111111111111111.py
+++ 111111111111111111.py
@@ -5,13 +5,11 @@ import plotly.express as px
 import pandas as pd
 import dash_table
 
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
-import plotly.express as px
-import pandas as pd
-import dash_table
-
 # Загрузка данных из файла
 file_path = 'C:/Users/xttna/PycharmProjects/pythonProject2/12.csv'  # Замените на путь к вашему файлу
 df = pd.read_csv(file_path)
+
+# Преобразование столбцов с датами
+df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])
+df['Delivery Date'] = pd.to_datetime(df['Delivery Date'], errors='coerce')
+df['Order Date'] = pd.to_datetime(df['Order Date'], errors='coerce')
