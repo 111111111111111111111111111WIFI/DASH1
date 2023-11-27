0eaf77576f63a6fd1520127916c8b3b35756ea9d
111111111111111111111111111WIFI <152071727+111111111111111111111111111WIFI@users.noreply.github.com>
2023-11-27 18:49:00 +1000

    1.3
    # Преобразование столбцов с датами

--- 111111111111111111.py
+++ 111111111111111111.py
@@ -9,3 +9,7 @@ import dash_table
 file_path = 'C:/Users/xttna/PycharmProjects/pythonProject2/12.csv'  # Замените на путь к вашему файлу
 df = pd.read_csv(file_path)
 
+# Преобразование столбцов с датами
+df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])
+df['Delivery Date'] = pd.to_datetime(df['Delivery Date'], errors='coerce')
+df['Order Date'] = pd.to_datetime(df['Order Date'], errors='coerce')
